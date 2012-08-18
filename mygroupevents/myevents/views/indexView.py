6a3b096c18bc8f3bd5406410d0a096bcf6f82084
jane <jinyunyan@hotmail.com>
2012-08-18 13:27:11 -0700

    login and singup

--- mygroupevents/myevents/views/indexView.py
+++ mygroupevents/myevents/views/indexView.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse,HttpResponseRedirect
 from myevents.models import * 
 from myevents.forms  import IndexForm
 import datetime 
-
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 def login(request):
     return render_to_response('myevents/login.html',{},context_instance=RequestContext(request))
@@ -16,12 +17,61 @@ def signup(request):
 def index(request):
     return render_to_response('myevents/index.html',{},context_instance=RequestContext(request))
 
+def guest(request):
+    return render_to_response('myevents/guest.html',{},context_instance=RequestContext(request))
+
+def profile(request):
+    if request.user.is_authenticated: 
+        return render_to_response('myevents/profile.html',{'user':request.user},context_instance=RequestContext(request))
+    else:
+        return render_to_response('myevents/login.html',{},context_instance=RequestContext(request))
+
+def userLogin(request):
+    user_email = request.POST['user_email']
+    password = request.POST['user_passwd']
+    u = authenticate(email=user_email, password=password)
+    if u is not None:
+        if u.is_active:
+            login(request, u)
+            return render_to_response('myevents/profile.html',{'user':request.user},context_instance=RequestContext(request))
+        else:
+            return render_to_response('myevents/error.html',{'message':'invalid password or email'},context_instance=RequestContext(request))
+
+    else:
+        # Return an 'invalid login' error message.
+        return render_to_response('myevents/error.html',{'message':'invalid login'},context_instance=RequestContext(request))
+
+def checkUserExistence(request):
+    if request.method=="GET":
+        user_email = request.GET.get('user_email')
+        try:
+            u = User.objects.get(email=user_email)
+            return HttpResponse({'is_existing':True}, mimetype='application/json')
+        except User.DoesNotExist:
+            return HttpResponse({'is_existing':False}, mimetype='application/json')
+    else:
+        return render_to_response('myevents/error.html',{'message':'the request type is wrong'},context_instance=RequestContext(request))
+
+def createUser(request):
+    if request.method == "POST":
+        user_email = request.POST.get('user_email')
+        user_name = request.POST.get('user_name')
+        user_passwd = request.POST.get('user_passwd')
+        try:
+            u = User.objects.get(email = user_email)
+            return render_to_response('myevents/error.html',{'message':'the email is already registered'},context_instance=RequestContext(request))
+        except User.DoesNotExist:
+            u = User.objects.create_user(user_name, user_email, user_passwd)
+        return render_to_response('myevents/profile.html',{'user':u},context_instance=RequestContext(request))
+    else:
+        return render_to_response('myevents/error.html',{'message':'the request type is wrong'},context_instance=RequestContext(request))
+            
+
 def createEvent(request):
     if request.method == "POST":
         #form = IndexForm(request.POST)
         #for optimization
         t0 = time.time()
-        
         #if form.is_valid():
         user_email = request.POST.get('user_email')
         event_name = request.POST.get('event_name')
