4dfc87642f2a80a029a2cfc86c46b394326058e8
JaneYan <jinyunyan@hotmail.com>
2012-09-09 18:50:05 -0700

    feature list

--- mygroupevents/myevents/views/experimentPostProcess.py
+++ mygroupevents/myevents/views/experimentPostProcess.py
@@ -3,6 +3,7 @@ from globalHeader import *
 import datetime
 import random
 import math
+from bs4 import BeautifulSoup
 
 def get_active_users():
 	us = user.objects.filter(is_active=True,is_registered=True)
@@ -22,14 +23,40 @@ def get_item_features(iid):
 	if ftype==CHOICE_SOURCE.YELP:
 		yelp_instance = yelp.objects.get(id=item_instance.foreign_id)	
 		print yelp.name, yelp.url
+		parser_yelp(yelp.url)
 
-
-def send_yelp_query():
-
+def parser_yelp(url):
+	base_url = 'http://www.yelp.com'
+	f = urllib.urlopen(url)
+	soup = BeautifulSoup(f.read())
+	f.close()
+	allcomments = []
+	page1 = soup.findAll('p','review_comment')
+	for p in page1:
+		#print p.contents
+		allcomments.append(p.contents)
+	#get next pages : has div pagination_controls
+	#get all next urls 
+	next_pages = soup.find('div','pagination_controls').find('table')
+	for nextp in next_pages.findAll('a'):
+		link =  nextp['href']
+		if link != None:
+			print link
+			newurl = base_url+link
+			newf = urllib.urlopen(newurl)
+			sp = BeautifulSoup(newf.read())
+			newf.close()
+			newcomment = sp.findAll('p','review_comment')
+			for p in newcomment:
+				allcomments.append(p.contents)				
+	
+	print 'total number of comments:%i'%len(allcomments)
+	for c in allcomments: 			
+		print c	
 
 # use positive fraction so far
 def get_vote_byuser_forplace(u,p):
-	
+	pass	
 
 def build_user_place_matrix():			
 	us = get_active_users()
