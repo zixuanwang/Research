b2a373a2445dc75426f668fa19958ae914d5dc7a
jane <jinyunyan@hotmail.com>
2012-09-18 15:10:22 -0700

    weight item only

--- groupevents_dataprocess/matlab_scripts/LPS-v2.2/code/Logistic.m
+++
@@ -1,6 +0,0 @@
-%
-% Logistic: calculate the logistic function of the input
-%
-function Output = Logistic(Input)
-    Output = 1./(1+exp(-Input));
-%EOF
--- groupevents_dataprocess/matlab_scripts/Test_lassoglm_itemOnly.m
+++ groupevents_dataprocess/matlab_scripts/Test_lassoglm_itemOnly.m
@@ -1,26 +1,21 @@
-% JINYUN 9/13/2012
+% JINYUN 9/18/2012
 
 % Requires definition of data files with size M*(N+1), where M data points
 % with N features, the last column is a vector of M
 % binary labels, with values +/-1
 
-files = dir('../user_instance_withmember/*.dat');
+files = dir('../user_instance/*.dat');
 k = numel(files);
 % one column = one weight vector
-allB = zeros(63,k); 
+allB = zeros(43,k); 
 for i = 1:numel(files)
-    filename = strcat('../user_instance_withmember/', files(i).name);
+    filename = strcat('../user_instance/', files(i).name);
     fprintf('\n\n ##### Problem %s\n\n', filename);
 	data = load(filename); 
 	% 10 folder cross validation, lasso has 2 cv, so here I set 5.
 	 [avg_train_acc, avg_test_acc,B_best]= CrossValidation_logits(5,data,'lassoglm');
-    %[avg_train_acc, avg_test_acc,B_best]= CrossValidation_logits(10,data,'logit');
     
   	fprintf('avg_train_acc: avg_test_acc =%6.2f\t%6.2f\n',avg_train_acc, avg_test_acc);
     allB(:,i) = B_best; 
     
 end
-
-
-  
-  
