b030a3fe01b35842f08a6acf44c0a5f2e0dc86a4
jane <jinyunyan@hotmail.com>
2012-10-03 18:55:50 -0700

    tpr, fpr

--- groupevents_dataprocess/matlab_scripts/Test_lassoglm_itemOnly.m
+++ groupevents_dataprocess/matlab_scripts/Test_lassoglm_itemOnly.m
@@ -4,22 +4,19 @@
 % with N features, the last column is a vector of M
 % binary labels, with values +/-1
 
-files = dir('../user_instance/*.dat');
-k = numel(files);
+%files = dir('../user_instance/*.dat');
+%k = numel(files);
 % one column = one weight vector
-allB = zeros(43,k); 
-for i = 1:numel(files)
-    filename = strcat('../user_instance/', files(i).name);
-    fprintf('\n\n ##### Problem %s\n\n', filename);
-	data = load(filename); 
+%allB = zeros(43,k); 
+%for i = 1:numel(files)
+    %filename = strcat('../user_instance/', files(i).name);
+    %fprintf('\n\n ##### Problem %s\n\n', filename);
+	%data = load(filename); 
 	% 10 folder cross validation, lasso has 2 cv, so here I set 5.
-	 [avg_train_acc, avg_test_acc,B_best]= CrossValidation_logits(5,data,'lassoglm');
+	% [avg_train_acc, avg_test_acc,B_best]= CrossValidation_logits(5,data,'lassoglm');
     
-  	fprintf('avg_train_acc: avg_test_acc =%6.2f\t%6.2f\n',avg_train_acc, avg_test_acc);
-    allB(:,i) = B_best; 
-   
-    
-    
+  %	fprintf('avg_train_acc: avg_test_acc =%6.2f\t%6.2f\n',avg_train_acc, avg_test_acc);
+  %  allB(:,i) = B_best;
     
 avg_train_precision = 0;
 avg_train_recall = 0;
--- groupevents_dataprocess/matlab_scripts/logistic_regression_past_event.m
+++ groupevents_dataprocess/matlab_scripts/logistic_regression_past_event.m
@@ -3,15 +3,16 @@
 
 function [avg_train_acc,avg_test_acc] = logistic_regression_past_event(threshold)
     
-    avg_train_precision = 0;
-    avg_train_recall = 0;
+    n_users =19;
+    train_tprs = zeros(n_users,1);
+    train_fprs = zeros(n_users,1);
    
-    avg_test_precision = 0;
-    avg_test_recall = 0;
+    test_tprs = zeros(n_users,1);
+    test_fprs = zeros(n_users,1);
+
     avg_train_acc=0;
     avg_test_acc=0;
     
-    n_users =19;
     train_accs =zeros(n_users,1);
     test_accs = zeros(n_users,1);
     for uid =1:n_users
@@ -19,15 +20,16 @@ function [avg_train_acc,avg_test_acc] = logistic_regression_past_event(threshold
         filename = strcat('../user_instance_pastevent/', int2str(uid));
         data = load(filename);
     
-        [train_precision,train_recall,train_acc,test_precision,test_recall, test_acc,B1] = measure_lassoglm_pthreshold(data,threshold);
+        %[train_precision,train_recall,train_acc,test_precision,test_recall, test_acc,B1] = measure_lassoglm_pthreshold(data,threshold);
+        [train_tpr,train_fpr,train_acc,test_tpr,test_fpr, test_acc,B1] = measure_lassoglm_pthreshold(data,threshold);
         fprintf('############# user %i \n',uid)
-        fprintf(' train_precision: %.4f, train_recall:%.4f, train_acc: %.4f\n',train_precision,train_recall,train_acc);
-        fprintf('test_precision: %.4f, test_recall: %.4f, test_acc: %.4f\n',test_precision,test_recall,test_acc);
-    
-        avg_train_precision =avg_train_precision + train_precision;
-        avg_train_recall =avg_train_recall + train_recall;
-        avg_test_precision =avg_test_precision + test_precision;
-        avg_test_recall =avg_test_recall + test_recall;
+        fprintf(' train_tpr: %.4f, train_fpr:%.4f, train_acc: %.4f\n',train_tpr,train_fpr,train_acc);
+        fprintf('test_tpr: %.4f, test_fpr: %.4f, test_acc: %.4f\n',test_tpr,test_fpr,test_acc);
+
+        train_tprs(uid) = train_tpr;
+        train_fprs(uid) = train_fpr;
+        test_tprs(uid) = test_tpr;
+        test_fprs(uid) = test_fpr; 
         
         avg_train_acc = avg_train_acc + train_acc;
         avg_test_acc = avg_test_acc + test_acc;
@@ -35,5 +37,6 @@ function [avg_train_acc,avg_test_acc] = logistic_regression_past_event(threshold
         train_accs(uid) = train_acc;
         test_accs(uid) = test_acc;
     end
-  
+   fprintf('avg_train_acc: %.4f, avg_test_acc: %.4f\n',avg_train_acc/n_users,avg_test_acc/n_users)
+    
 end
